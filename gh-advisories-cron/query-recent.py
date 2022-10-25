02358d17802fb7d21d23dfc026959e0596dcd70e
Jake Perret <djakeperret@gmail.com>
2022-10-25 19:50:52 -0400

    create query by recent vuls file

---
+++ gh-advisories-cron/query-recent.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+
+
+# GitHub personal access token (classic)
+# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-personal-access-token-classic
+token = ""
+
+url = 'https://api.github.com/graphql'
+
+query = """ {
+securityVulnerabilities(first:100) {
+    nodes {
+      advisory {
+        cwes(first:100){
+          nodes {
+            cweId
+          }
+        }
+        identifiers {
+          value
+          type
+        }
+      }
+      package {
+        name
+        ecosystem
+      }
+      severity
+      vulnerableVersionRange
+    }
+  }
+}
+"""
+
+auth_headers = {"Authorization": "Bearer " + token}
+
+r = requests.post(url, json={'query': query}, headers=auth_headers)
+print(json.dumps(json.loads(r.text), indent=2))
