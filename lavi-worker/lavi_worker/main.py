061539ad1abb16366e8b19c2363cafc395f930ad
Levi Lutz <levi6900@icloud.com>
2022-10-30 01:14:24 -0400

    Wire lavi-worker into meta db

--- lavi-worker/lavi_worker/main.py
+++ lavi-worker/lavi_worker/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 from lavi_worker import config
+from lavi_worker.meta_db_client import get_greeting
+from lavi_worker.meta_db_client.client import protos
 
 app = FastAPI(root_path=config.EXPECTED_PREFIX)
 
@@ -10,3 +12,15 @@ app = FastAPI(root_path=config.EXPECTED_PREFIX)
 def ping() -> str:
     """Ping pong."""
     return "pong"
+
+
+@app.get("/greeting", response_class=PlainTextResponse)
+def greeting(name: str) -> str:
+    """Get a greeting from the meta db grpc server."""
+    return get_greeting(name=name)
+
+
+@app.get("/test", response_class=PlainTextResponse)
+def test() -> str:
+    """Testing code."""
+    return str(dir(protos))
---
+++ lavi-worker/lavi_worker/meta_db_client/client.py
@@ -0,0 +1,13 @@
+import grpc
+import grpc.experimental
+
+protos, services = grpc.protos_and_services(
+    "lavi_worker/meta_db_client/helloworld.proto"
+)
+
+
+def get_greeting(name: str) -> str:
+    """Get a greeting from the grpc server."""
+    req = protos.HelloRequest(name=name)
+    resp = services.Greeter.SayHello(req, "meta-db:80", insecure=True)
+    return resp.message
