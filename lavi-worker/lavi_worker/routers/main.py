d1450eb60ca8796679afddafd1fc0f798dc4f8ed
Levi Lutz <levi6900@icloud.com>
2022-11-01 21:02:09 -0400

    Add stub routers

--- lavi-worker/lavi_worker/routers/main.py
+++ lavi-worker/lavi_worker/routers/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
 from lavi_worker import config
+from lavi_worker.routers import external, internal
+
 
 app = FastAPI(root_path=config.EXPECTED_PREFIX)
+
+# Add routers
+app.include_router(external.router)
+app.include_router(internal.router, prefix="/internal")
 
 
 @app.get("/ping", response_class=PlainTextResponse)
