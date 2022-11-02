75bcbb31d377612be440000453d6a22048a2d819
Jake Perret <djakeperret@gmail.com>
2022-11-01 23:06:52 -0400

    parse response

--- gh-advisories-cron/query-recent.py
+++ gh-advisories-cron/query-recent.py
@@ -7,15 +7,13 @@ repository = "PIP"
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-personal-access-token-classic
 token = ""
 
+# Get vulnerabilities after:
 last_cursor = "Y3Vyc29yOnYyOpK5MjAyMi0xMC0zMVQxODo0Mjo1OC0wNDowMM2LOA=="
-url = 'https://api.github.com/graphql'
 
-# TODO: Not sure if want to query securityAdvisories or securityVulnerabilities
-# Edge gives cursor and node'
 # Keep repository in query so that only relevant vulnerabilities are returned
 query = """ 
 {
-  securityVulnerabilities(first:3, ecosystem: """ + repository + """, after: \"""" + last_cursor + """\") {
+  securityVulnerabilities(first:10, ecosystem: """ + repository + """, after: \"""" + last_cursor + """\") {
     edges {
       cursor
       node {
@@ -25,6 +23,8 @@ query = """
               cweId
             }
           }
+          summary
+          permalink
           identifiers {
             value
             type
@@ -35,6 +35,7 @@ query = """
           ecosystem
         }
         severity
+        updatedAt
         vulnerableVersionRange
       }
     }
@@ -45,7 +46,45 @@ query = """
 }
 """
 
+# Add authorization token to headers
 auth_headers = {"Authorization": "Bearer " + token}
+response = requests.post("https://api.github.com/graphql", json={'query': query}, headers=auth_headers)
 
-r = requests.post(url, json={'query': query}, headers=auth_headers)
-print(json.dumps(json.loads(r.text), indent=2))
+# Print returned JSON
+print(json.dumps(json.loads(response.text), indent=2))
+
+# Save for next query
+last_cursor_received = json.loads(response.text)['data']['securityVulnerabilities']['pageInfo']['endCursor']
+
+# Variables to help with spelling errors
+CVE_ID = "cve_id"
+severity = "severity"
+description = "description"
+CWES = "cwes"
+url = "url"
+repo = "repo_name"
+package = "pkg_name"
+vulnerableVersionRange = "vulnVersRange"
+
+# Parse each vulnerability returned
+parsed_vulnerabilities = []
+for gh_vuln_edge in json.loads(response.text)['data']['securityVulnerabilities']['edges']:
+	parsed_vuln = {}
+	gh_vuln = gh_vuln_edge['node']
+	cursor = gh_vuln_edge['cursor']  # not sure if needed
+
+	parsed_vuln[CVE_ID] = next((item['value'] for item in gh_vuln['advisory']['identifiers'] if item['type'] == 'CVE'), None)
+	if parsed_vuln[CVE_ID] is None:
+		# No CVE, don't add to database
+		continue
+	parsed_vuln[severity] = gh_vuln['severity']
+	parsed_vuln[description] = gh_vuln['advisory']['summary']
+	parsed_vuln[CWES] = ",".join([cwe_node['cweId'] for cwe_node in gh_vuln['advisory']['cwes']['nodes']])
+	parsed_vuln[url] = gh_vuln['advisory']['permalink']
+	parsed_vuln[repo] = gh_vuln['package']['ecosystem']
+	parsed_vuln[package] = gh_vuln['package']['name']
+	parsed_vuln[vulnerableVersionRange] = gh_vuln['vulnerableVersionRange']
+
+	parsed_vulnerabilities.append(parsed_vuln)
+
+print(parsed_vulnerabilities)
