1819a33318f20b9e87df266a61b294a2ff40bf46
Levi Lutz <levi6900@icloud.com>
2022-11-03 18:37:15 -0400

    black

--- lavi-worker/lavi_worker/internal/updates.py
+++ lavi-worker/lavi_worker/internal/updates.py
@@ -9,6 +9,7 @@ from lavi_worker.daos.database import get_db_tx
 # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-personal-access-token-classic
 GitHub_access_token = ""
 
+
 async def is_db_initialized() -> bool:
     """Return whether the database has been initialized yet."""
     # Just check if the main table has been made
@@ -128,13 +129,13 @@ async def scrape_vulnerabilities() -> None:
         def get_last_cursor():
             try:
                 # TODO can I persist this data without files?
-                with open(last_cursor_file, 'r+') as f:
+                with open(last_cursor_file, "r+") as f:
                     return f.read()
             except FileNotFoundError:
                 return None
 
         def save_last_cursor(last_cursor_received):
-            with open(last_cursor_file, 'w') as f:
+            with open(last_cursor_file, "w") as f:
                 f.write(last_cursor_received)
 
         # Get vulnerabilities after:
@@ -142,12 +143,22 @@ async def scrape_vulnerabilities() -> None:
 
         # Repeats until there are no new vulnerabilities
         while True:
-            query_type = "securityVulnerabilities(first:100, ecosystem: " + repository \
-                     + ("" if last_cursor is None or last_cursor == "" else ", after: \"" + last_cursor + "\"") \
-                     + ", orderBy: {field: UPDATED_AT, direction: ASC})"
+            query_type = (
+                "securityVulnerabilities(first:100, ecosystem: "
+                + repository
+                + (
+                    ""
+                    if last_cursor is None or last_cursor == ""
+                    else ', after: "' + last_cursor + '"'
+                )
+                + ", orderBy: {field: UPDATED_AT, direction: ASC})"
+            )
 
-            query = """ 
-            {""" + query_type + """
+            query = (
+                """ 
+            {"""
+                + query_type
+                + """
                {
                 edges {
                   cursor
@@ -180,15 +191,22 @@ async def scrape_vulnerabilities() -> None:
               }
             }
             """
+            )
 
             # Add authorization token to headers
-            response = httpx.post("https://api.github.com/graphql", json={'query': query}, headers=auth_headers)
+            response = httpx.post(
+                "https://api.github.com/graphql",
+                json={"query": query},
+                headers=auth_headers,
+            )
 
             # Print returned JSON
             # print(json.dumps(json.loads(response.text), indent=2))
 
             # Save for next query
-            last_cursor = json.loads(response.text)['data']['securityVulnerabilities']['pageInfo']['endCursor']
+            last_cursor = json.loads(response.text)["data"]["securityVulnerabilities"][
+                "pageInfo"
+            ]["endCursor"]
 
             if last_cursor is None:
                 print("no newer vulns")
@@ -198,23 +216,46 @@ async def scrape_vulnerabilities() -> None:
 
             # Parse each vulnerability returned
             parsed_vulnerabilities = []
-            for gh_vuln_edge in json.loads(response.text)['data']['securityVulnerabilities']['edges']:
+            for gh_vuln_edge in json.loads(response.text)["data"][
+                "securityVulnerabilities"
+            ]["edges"]:
                 parsed_vuln = {}
-                gh_vuln = gh_vuln_edge['node']
+                gh_vuln = gh_vuln_edge["node"]
 
-                cve_id = next((item['value'] for item in gh_vuln['advisory']['identifiers'] if item['type'] == 'CVE'), None)
+                cve_id = next(
+                    (
+                        item["value"]
+                        for item in gh_vuln["advisory"]["identifiers"]
+                        if item["type"] == "CVE"
+                    ),
+                    None,
+                )
                 if cve_id is None:
                     # No CVE, don't add to database
                     continue
 
-                severity = gh_vuln['severity']
-                description = gh_vuln['advisory']['summary']
-                cwes = ",".join([cwe_node['cweId'] for cwe_node in gh_vuln['advisory']['cwes']['nodes']])
-                url = gh_vuln['advisory']['permalink']
-                repo_name = gh_vuln['package']['ecosystem']
-                pkg_name = gh_vuln['package']['name']
-                pkg_vers = gh_vuln['vulnerableVersionRange']
+                severity = gh_vuln["severity"]
+                description = gh_vuln["advisory"]["summary"]
+                cwes = ",".join(
+                    [
+                        cwe_node["cweId"]
+                        for cwe_node in gh_vuln["advisory"]["cwes"]["nodes"]
+                    ]
+                )
+                url = gh_vuln["advisory"]["permalink"]
+                repo_name = gh_vuln["package"]["ecosystem"]
+                pkg_name = gh_vuln["package"]["name"]
+                pkg_vers = gh_vuln["vulnerableVersionRange"]
 
                 # TODO pkg_vers is range
                 # TODO replace insert_single_vulnerability()?
-                insert_single_vulnerability(cve_id, url, repo_name, pkg_name, pkg_vers, severity, description, cwes)
+                insert_single_vulnerability(
+                    cve_id,
+                    url,
+                    repo_name,
+                    pkg_name,
+                    pkg_vers,
+                    severity,
+                    description,
+                    cwes,
+                )
