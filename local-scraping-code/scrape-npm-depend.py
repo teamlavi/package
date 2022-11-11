4dcc870018f34467352eb5ff7ae7003bd2dc09d7
Jake Perret <djakeperret@gmail.com>
2022-11-11 16:56:24 -0500

    npm dependency scraping; cache completed trees; get newest versions

--- local-scraping-code/scrape-npm-depend.py
+++ local-scraping-code/scrape-npm-depend.py
@@ -1,20 +1,44 @@
 import httpx
 import json
+import os
 
-# TODO: create list of fully searched trees to not search again
-def get_dependencies(package, version, tab):
+# TODO: include dev or other types of dependencies?
+
+# cache completed trees
+complete_trees = {}
+
+
+def get_recent_vers(package):
+	return os.popen("npm view " + package + " version").read().strip()
+
+
+def get_dependencies(package, version, tab=''):
 	if version[0] == "^":
-		version = version[1:]
-	print(tab, package, version)
-	data = httpx.get("https://registry.npmjs.org/"+package+"/"+version).text
-	if 'dependencies' in json.loads(data).keys():
-		dependency_dict =  json.loads(data)['dependencies']
-	elif 'devDependencies' in json.loads(data).keys():
-		dependency_dict = json.loads(data)['devDependencies']
-	else:
-		# print(json.loads(data).keys())
-		return
-	for p in dependency_dict:
-		get_dependencies(p, dependency_dict[p], tab+"\t")
+		version = get_recent_vers(package)
 
-get_dependencies('eslint-plugin-import', '1.0.0', '')
+	print(tab, package, version)
+
+	if package+version in complete_trees:
+		return complete_trees[package+version]
+
+	data = httpx.get("https://registry.npmjs.org/"+package+"/"+version).text
+
+	if 'dependencies' not in json.loads(data).keys():
+		# print(json.loads(data).keys())
+		complete_trees[package+version] = {}
+		return {}
+
+	dependency_list = json.loads(data)['dependencies'].keys()
+	this_tree = {}
+	for p in dependency_list:
+		v = get_recent_vers(p)
+		this_tree[p+v] = get_dependencies(p, v, tab+"\t")
+	complete_trees[package+version] = this_tree
+	# print(this_tree)
+	return this_tree
+
+# print(get_dependencies('react', '18.2.0'))
+
+print(get_dependencies('gl-nodeutilities-test', '^1.0.0'))
+
+# print(complete_trees)
