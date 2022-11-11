3fec590a2c4fafa2e8f0f359f42b972317906642
Jake Perret <djakeperret@gmail.com>
2022-11-10 22:23:25 -0500

    Add recursive dependency search

--- local-scraping-code/scrape-npm-depend.py
+++ local-scraping-code/scrape-npm-depend.py
@@ -1,5 +1,20 @@
 import httpx
 import json
 
-data = httpx.get("https://registry.npmjs.org/eslint-plugin-import/1.0.0").text
-print(json.loads(data)['dependencies'])
+# TODO: create list of fully searched trees to not search again
+def get_dependencies(package, version, tab):
+	if version[0] == "^":
+		version = version[1:]
+	print(tab, package, version)
+	data = httpx.get("https://registry.npmjs.org/"+package+"/"+version).text
+	if 'dependencies' in json.loads(data).keys():
+		dependency_dict =  json.loads(data)['dependencies']
+	elif 'devDependencies' in json.loads(data).keys():
+		dependency_dict = json.loads(data)['devDependencies']
+	else:
+		# print(json.loads(data).keys())
+		return
+	for p in dependency_dict:
+		get_dependencies(p, dependency_dict[p], tab+"\t")
+
+get_dependencies('eslint-plugin-import', '1.0.0', '')
