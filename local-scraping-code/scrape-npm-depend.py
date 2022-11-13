2c5391ec7920fed93d1fde0d4a72b56d2a7ed532
Jake Perret <djakeperret@gmail.com>
2022-11-13 14:24:11 -0500

    formatting

--- local-scraping-code/scrape-npm-depend.py
+++ local-scraping-code/scrape-npm-depend.py
@@ -9,36 +9,36 @@ complete_trees = {}
 
 
 def get_recent_vers(package):
-	return os.popen("npm view " + package + " version").read().strip()
+    return os.popen("npm view " + package + " version").read().strip()
 
 
-def get_dependencies(package, version, tab=''):
-	if version[0] == "^":
-		version = get_recent_vers(package)
+def get_dependencies(package, version, tab=""):
+    if version[0] == "^":
+        version = get_recent_vers(package)
 
-	print(tab, package, version)
+    print(tab, package, version)
 
-	if package+version in complete_trees:
-		return complete_trees[package+version]
+    if package + version in complete_trees:
+        return complete_trees[package + version]
 
-	data = httpx.get("https://registry.npmjs.org/"+package+"/"+version).text
+    data = httpx.get("https://registry.npmjs.org/" + package + "/" + version).text
 
-	if 'dependencies' not in json.loads(data).keys():
-		# print(json.loads(data).keys())
-		complete_trees[package+version] = {}
-		return {}
+    if "dependencies" not in json.loads(data).keys():
+        complete_trees[package + version] = {}
+        return {}
 
-	dependency_list = json.loads(data)['dependencies'].keys()
-	this_tree = {}
-	for p in dependency_list:
-		v = get_recent_vers(p)
-		this_tree[p+v] = get_dependencies(p, v, tab+"\t")
-	complete_trees[package+version] = this_tree
-	# print(this_tree)
-	return this_tree
+    dependency_list = json.loads(data)["dependencies"].keys()
+    this_tree = {}
+    for p in dependency_list:
+        v = get_recent_vers(p)
+        this_tree[p + v] = get_dependencies(p, v, tab + "\t")
+    complete_trees[package + version] = this_tree
+    # print(this_tree)
+    return this_tree
+
 
 # print(get_dependencies('react', '18.2.0'))
 
-print(get_dependencies('gl-nodeutilities-test', '^1.0.0'))
+print(get_dependencies("gl-nodeutilities-test", "^1.0.0"))
 
-# print(complete_trees)
+print(complete_trees)
