ff2ef85fe5a2d337c878844969087781b0acfb09
Jake Perret <djakeperret@gmail.com>
2022-11-14 15:13:18 -0500

    Add template npm scraping code

--- repo-worker/repo_worker/scrapers/scraper_npm.py
+++ repo-worker/repo_worker/scrapers/scraper_npm.py
@@ -17,4 +17,12 @@ class NpmScraper(object):
     @staticmethod
     def generate_dependency_tree(package: str, version: str) -> TreeNode:
         """Given a repository, package, and version, return a conflict-free dep tree."""
-        raise NotImplementedError
+        # TODO: install lavi-cli
+        for pkg_name in ["lodash"]:
+            pkg_vers = await get_most_recent_vers("npm", pkg_name)
+            cmd = f'lavi npm --package="{pkg_name}" --version="{pkg_vers}" -w'
+            request = os.popen(cmd).read()
+            print(request)
+            with open("cds.json") as f:
+                print(f.read())
+                await insert_single_dependency_tree("npm", pkg_name, pkg_vers, f.read())
