08d3fcf4d4540b2bb17f2cb5fcedf388fd361620
Levi Lutz <levi6900@icloud.com>
2022-11-13 22:53:32 -0500

    Impls for the cli funcs

--- repo-worker/repo_worker/core/cli_funcs.py
+++ repo-worker/repo_worker/core/cli_funcs.py
@@ -1,11 +1,20 @@
+from repo_worker.scrapers import repo_scrapers
+
+
 def list_packages(repo: str, limit: int | None = None) -> None:
     """Handle CLI runs of list-packages"""
-    raise NotImplementedError
+    scraper = repo_scrapers[repo]
+    packages = scraper.list_packages(repo=repo, limit=limit)
+    for package in packages:
+        print(package)
 
 
 def list_package_versions(repo: str, package: str, limit: int | None = None) -> None:
     """Handle CLI runs of list-package-versions."""
-    raise NotImplementedError
+    scraper = repo_scrapers[repo]
+    versions = scraper.list_package_versions(repo=repo, package=package, limit=limit)
+    for version in versions:
+        print(version)
 
 
 def generate_tree(repo: str, package: str, version: str) -> None:
