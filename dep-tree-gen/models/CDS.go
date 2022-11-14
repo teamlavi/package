0d2b1f792c4a02ee277e449422ba89a6f7bd9198
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-11-13 23:20:26 -0500

    hold

--- dep-tree-gen/models/CDS.go
+++ dep-tree-gen/models/CDS.go
@@ -14,3 +14,30 @@ type CDSNode struct {
 	Version      string   `json:"version,omitempty"`
 	Dependencies []string `json:"dependencies"`
 }
+
+func contains(val string, arr []string) bool {
+	for _, a := range arr {
+		if val == a {
+			return true
+		}
+	}
+	return false
+}
+
+func (c CDS) IsRoot(id string) bool {
+	return contains(id, c.Root.Dependencies)
+}
+
+func (c CDS) FindRootParents(id string) []CDSNode {
+	if c.IsRoot(id) {
+		return []CDSNode{c.Nodes[id]}
+	}
+
+	out := []CDSNode{}
+	for _, node := range c.Nodes {
+		if contains(id, node.Dependencies) {
+			out = append(out, c.FindRootParents(node.ID)...)
+		}
+	}
+	return out
+}
