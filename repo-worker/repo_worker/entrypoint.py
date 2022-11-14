3cea24f616ca06ee1628193aa484875efb808c40
Levi Lutz <levi6900@icloud.com>
2022-11-13 21:32:23 -0500

    Sub out command-line interface

--- repo-worker/repo_worker/entrypoint.py
+++ repo-worker/repo_worker/entrypoint.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations  # Postponed annotation evaluation, remove once 3.11
+
+import argparse
 from typing import Dict, List, Protocol
 
 
 class TreeNode(object):
-    def __init__(self, univ_hash, children):
+    def __init__(
+        self,
+        repo: str,
+        package: str,
+        version: str,
+        children: List[TreeNode] | None = None,
+    ):
         """Build a tree node given a hash and children."""
-        self.univ_hash = univ_hash
+        self.repo = repo
+        self.package = package
+        self.version = version
         self.children = children
 
-    def add_child(self, child) -> None:
+    def add_child(self, child: TreeNode) -> None:
         """Add a child node."""
         self.children.append(child)
+
+    def add_children(self, children: List[TreeNode]) -> None:
+        """Add multiple children."""
+        self.children.extend(children)
 
     def as_json(self) -> str:
         """Return the tree represented as json."""
@@ -36,3 +51,50 @@ supported_repos: Dict[str, RepoScraper] = {
     "npm": RepoScraper,
     "pypi": RepoScraper,
 }
+
+
+def parse_cmd_args():
+    """Parse command-line arguments."""
+    # Main parser
+    parser = argparse.ArgumentParser(prog="entrypoint")
+    parser.add_argument("-m", "--mode", choices=["cli", "redis"], required=True)
+    subparsers = parser.add_subparsers(help="sub-command help", dest="command")
+
+    # list-packages subcommand - List packages for a given repository
+    list_packages = subparsers.add_parser("list-packages", help="list-packages help")
+    # TODO add options here dynamically from supported_repos
+    list_packages.add_argument("-r", "--repo", action="store", required=True)
+
+    # list-package-versions subcommand - List versions a stream of (repo, package)
+    subparsers.add_parser(
+        "list-package-versions", help="list-package-versions help"
+    )
+
+    # generate-tree subcommand - Generate a conflict-free dep tree given all data
+    subparsers.add_parser("generate-tree", help="generate-tree help")
+
+    # Do the parse
+    return parser.parse_args()
+
+
+def main() -> None:
+    """Accept command-line args, run as requested."""
+    args = parse_cmd_args()
+
+    if args.command is None:
+        raise Exception("No command provided")
+
+    if args.mode == "cli":
+        # Pull from stdin and push to stdout
+        # Runs a single time
+        print("Running in cli mode")
+    elif args.mode == "redis":
+        # Pull and push data to redis store
+        # Runs list-packages once, or the other commands on loop
+        print("Running in redis mode")
+    else:
+        raise Exception(f"Unrecognized execution mode: {args.mode}")
+
+
+if __name__ == "__main__":
+    main()
