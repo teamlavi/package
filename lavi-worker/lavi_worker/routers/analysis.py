cbbaa5fa46990b8584c478b1f4e19a3462909a09
edson <edcortes@terpmail.umd.edu>
2022-11-13 21:26:49 -0500

    Added Numeration comments to format lava endpoints

--- lavi-worker/lavi_worker/routers/analysis.py
+++ lavi-worker/lavi_worker/routers/analysis.py
@@ -2,5 +2,35 @@ from fastapi import APIRouter
 
 from lavi_worker.routers import api_models
 
-
 router = APIRouter(tags=["analysis"])
+
+#1.) affectedCount - For vulnerabilities found in queried packages return a list with the number of packages affected by each vulnerability.
+
+
+#2.) Count - Number of packages.
+
+
+#3.) countDependencies - Returns list of how many other packages each package relies on.
+
+
+#4.) countVul - Number of vulnerable packages.
+
+
+#5.) Depth - Returns list of how deep each vulnerability was from the top level package (how many dependencies deep).
+
+
+#6.) numDownloads - Returns a list with the number of downloads for each package included.
+
+
+#7.) Severities - Return list of vulnerable packages and severity for each vulnerability.
+
+
+#8.) Types - Returns CSV with CWEs and a count of how many vulnerabilities for each CWE
+
+
+#9.) vulnerablePackages - Return list of vulnerable packages.
+
+
+
+
+
