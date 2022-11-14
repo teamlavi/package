a2e663e1e107b0e51ab8ecd19cf3103bcd9c65cb
Levi Lutz <levi6900@icloud.com>
2022-11-14 03:13:05 -0500

    Stub npm and pip scrapers

--- repo-worker/repo_worker/scrapers/scraper_npm.py
+++ repo-worker/repo_worker/scrapers/scraper_npm.py
@@ -1,30 +1,20 @@
-from typing import Dict, List, Protocol
+from typing import List
 
-from repo_worker.scrapers.scraper_dud import DudScraper
-from repo_worker.scrapers.scraper_npm import NpmScraper
-from repo_worker.scrapers.scraper_pip import PipScraper
 from repo_worker.utils import TreeNode
 
 
-class RepoScraper(Protocol):
+class NpmScraper(object):
     @staticmethod
     def list_packages(limit: int | None = None) -> List[str]:
         """Given a repository, return a list of its packages."""
-        ...
+        raise NotImplementedError
 
     @staticmethod
     def list_package_versions(package: str, limit: int | None = None) -> List[str]:
         """Given a repository and package, return a list of available versions."""
-        ...
+        raise NotImplementedError
 
     @staticmethod
     def generate_dependency_tree(package: str, version: str) -> TreeNode:
         """Given a repository, package, and version, return a conflict-free dep tree."""
-        ...
-
-
-repo_scrapers: Dict[str, RepoScraper] = {
-    "dud": DudScraper(),
-    "npm": NpmScraper(),
-    "pip": PipScraper(),
-}
+        raise NotImplementedError
