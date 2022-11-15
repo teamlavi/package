05b46fef5973a239d33cf219b91ccac6387a8f83
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-11-15 16:54:33 -0500

    working on it lmao

--- lava-cli/internal/client/client.go
+++ lava-cli/internal/client/client.go
@@ -1,12 +1,13 @@
 package client
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"lava/internal/models"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -28,20 +29,64 @@ func New(remote ...string) *Client {
 	}
 }
 
+// ANY FUNCTION CALLED IN HERE MUST PANIC
+// DO NOT USE LOG.FATAL
 func (c *Client) Run(cmd *cobra.Command, endpoint string) {
+	var resp *http.Response
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("LAVA failed. Error: %s\n", err)
+			fmt.Println("Attempting to write api response body to a file for inspection and future use")
+			out, err := os.Create(fmt.Sprintf("lava-response-%s.txt", randSeq(10)))
+			if err != nil {
+				fmt.Println(1, err)
+				log.Fatal("Failed to create file")
+			}
+			defer out.Close()
+			if _, err := io.Copy(out, resp.Body); err != nil {
+				fmt.Println(2, err)
+				log.Fatal("Failed to write to file", err)
+			}
+			os.Exit(1)
+		}
+	}()
 	request := models.BuildLavaRequest(cmd)
-	fmt.Println(request)
-	c.sendPost(request, endpoint)
+	resp, err := c.sendPost(request, endpoint)
+	if err != nil {
+		panic("Request to lava api failed")
+	}
+	lavaResp := c.handleResp(resp)
+	fmt.Println(lavaResp)
 }
 
+// sends the request to lava api
+// in the future, this will return a job id that will be picked up by another function
+// to poll the get endpoint
 func (c *Client) sendPost(body *models.LavaRequest, endpoint string) (*http.Response, error) {
-	json_data, err := json.Marshal(body)
+	// TODO: THIS IS FOR POSTING WHEN THE API IS READY
+	// json_data, err := json.Marshal(body)
 
-	if err != nil {
-		log.Fatal("unknown error occured while sending post request")
+	// if err != nil {
+	// 	log.Fatal("unknown error occured while sending post request")
+	// }
+
+	// resp, err := http.Post(fmt.Sprintf("%s/%s", c.remote, endpoint), "application/json",
+	// 	bytes.NewBuffer(json_data))
+
+	resp, err := http.Get(fmt.Sprintf("%s/%s", c.remote, endpoint))
+	return resp, err
+}
+
+// handles the response from the lava api
+func (c *Client) handleResp(resp *http.Response) models.LavaResponse {
+
+	var res models.LavaResponse
+
+	err := json.NewDecoder(resp.Body).Decode(&res)
+	fmt.Println(res, err)
+	if err == nil {
+		panic("Could not decode api response body")
 	}
 
-	resp, err := http.Post(fmt.Sprintf("%s/%s", c.remote, endpoint), "application/json",
-		bytes.NewBuffer(json_data))
-	return resp, err
+	return res
 }
---
+++ lava-cli/internal/client/utils.go
@@ -0,0 +1,13 @@
+package client
+
+import "math/rand"
+
+var letters = []rune("1234567890")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
