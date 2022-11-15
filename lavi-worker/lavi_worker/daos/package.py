1a5c114ba0972b0db0f0dcfce92f52bf30c79823
Jake Perret <djakeperret@gmail.com>
2022-11-15 18:37:29 -0500

    initial new pacakge.py code

--- lavi-worker/lavi_worker/daos/package.py
+++ lavi-worker/lavi_worker/daos/package.py
@@ -1,19 +1,23 @@
-from attrs import define
-
 from lavi_worker.daos.database import Transaction
-from lavi_worker.utils import generate_universal_hash
+import httpx
+import json
+from typing import List
 
 
-@define(frozen=True)
-class PACKAGE:
-    repo_name: str
-    pkg_name: str
-    univ_hash: str
+class SemVer:
     major_vers: int
     minor_vers: int
     patch_vers: int
-    num_downloads: int
-    s3_bucket: str
+
+    def __init__(
+        self, major_vers: int | str, minor_vers: int | str, patch_vers: int | str
+    ):
+        self.major_vers = int(major_vers)
+        self.minor_vers = int(minor_vers)
+        self.patch_vers = int(patch_vers)
+
+    def __repr__(self):
+        return f"{self.major_vers}.{self.minor_vers}.{self.patch_vers}"
 
 
 async def create(
@@ -26,76 +30,90 @@ async def create(
     num_downloads: int | None,
     s3_bucket: str | None,
 ) -> None:
-    """Create a CVE object in the database, return nothing if successful."""
-    univ_hash = generate_universal_hash(
-        repo_name,
-        pkg_name,
-        str(major_vers) + "." + str(minor_vers) + "." + str(patch_vers),
-    )
-    async with tx.cursor() as cur:
-        if await vers_exists(
-            tx, repo_name, pkg_name, major_vers, minor_vers, patch_vers
-        ):
-            return
-        else:
-            await cur.execute(
-                """
-                INSERT INTO package
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-            """,
-                (
-                    univ_hash,
-                    repo_name,
-                    pkg_name,
-                    major_vers,
-                    minor_vers,
-                    patch_vers,
-                    num_downloads,
-                    s3_bucket,
-                ),
-            )
+    return
+
+
+async def list_package_versions_npm(
+    package: str, limit: int | None = None
+) -> List[SemVer]:
+    """Given a repository and package, return a list of available versions."""
+    try:
+        resp = httpx.get(
+            f"https://registry.npmjs.org/{package}",
+        )
+        resp.raise_for_status()
+        version_list = list(json.loads(resp.text)["versions"])
+        if isinstance(version_list, str) and "-" in version_list:
+            return []
+        elif isinstance(version_list, str):
+            version_list = [version_list]
+        elif isinstance(version_list[0], list):
+            version_list = version_list[0]
+
+        res_versions: List[SemVer] = []
+
+        for vers in version_list:
+            if limit is not None and len(res_versions) >= limit:
+                break
+            elif vers.replace(".", "").isnumeric():
+                # checks if there are characters in version number
+                while vers.count(".") < 2:
+                    # if no minor or patch version included
+                    vers += ".0"
+                res_versions.append(*vers.split("."))
+        return res_versions
+    except Exception as e:
+        print(f"Unable to interpret versions for {package}", e)
+        return []
+
+
+async def list_package_versions_pip(
+    package: str, limit: int | None = None
+) -> List[SemVer]:
+    """Given a repository and package, return a list of available versions."""
+    page2 = f"https://pypi.org/pypi/{package}/json"
+
+    all_versions = json.loads(httpx.get(page2).text)["releases"].keys()
+    res_versions: List[str] = []
+    for vers in all_versions:
+        if limit is not None and len(res_versions) >= limit:
+            break
+        elif vers.replace(".", "").isnumeric():
+            # checks if there are characters in version number
+            while vers.count(".") < 2:
+                # if no minor or patch version included
+                vers += ".0"
+            res_versions.append(vers)
+    return res_versions
 
 
 async def get_vers_less_than_eql(
-    tx: Transaction,
+    tx: Transaction | None,
     repo_name: str,
     pkg_name: str,
-    major_vers: str,
-    minor_vers: str,
-    patch_vers: str,
+    major_vers: int | str,
+    minor_vers: int | str,
+    patch_vers: int | str,
 ) -> list[str]:
-    async with tx.cursor() as cur:
-        await cur.execute(
-            """
-                SELECT major_vers,
-                       minor_vers,
-                       patch_vers
-                FROM PACKAGE
-                WHERE repo_name=%s AND pkg_name=%s
-                  AND (major_vers<%s
-                       OR (major_vers=%s
-                           AND minor_vers<%s)
-                       OR (major_vers=%s
-                           AND minor_vers=%s
-                           AND patch_vers <=%s))
-            """,
-            (
-                repo_name,
-                pkg_name,
-                major_vers,
-                major_vers,
-                minor_vers,
-                major_vers,
-                minor_vers,
-                patch_vers,
-            ),
-        )
-        rows = await cur.fetchall()
-        vers_list = []
-        for r in rows:
-            vers = str(r[0]) + "." + str(r[1]) + "." + str(r[2])
-            vers_list.append(vers)
-        return vers_list
+    if repo_name == "npm":
+        all_vers = await list_package_versions_npm(pkg_name)
+    elif repo_name == "pip":
+        all_vers = await list_package_versions_npm(pkg_name)
+    else:
+        all_vers = []
+    res: List[str] = []
+    for vers in all_vers:
+        if vers.major_vers < int(major_vers):
+            res.append(str(vers))
+        elif vers.major_vers == int(major_vers) and vers.minor_vers < int(minor_vers):
+            res.append(str(vers))
+        elif (
+            vers.major_vers == int(major_vers)
+            and vers.minor_vers == int(minor_vers)
+            and vers.patch_vers < int(patch_vers)
+        ):
+            res.append(str(vers))
+    return res
 
 
 async def get_vers_less_than(
@@ -106,38 +124,7 @@ async def get_vers_less_than(
     minor_vers: str,
     patch_vers: str,
 ) -> list[str]:
-    async with tx.cursor() as cur:
-        await cur.execute(
-            """
-                SELECT major_vers,
-                       minor_vers,
-                       patch_vers
-                FROM PACKAGE
-                WHERE repo_name=%s AND pkg_name=%s
-                  AND (major_vers<%s
-                       OR (major_vers=%s
-                           AND minor_vers<%s)
-                       OR (major_vers=%s
-                           AND minor_vers=%s
-                           AND patch_vers <%s))
-            """,
-            (
-                repo_name,
-                pkg_name,
-                major_vers,
-                major_vers,
-                minor_vers,
-                major_vers,
-                minor_vers,
-                patch_vers,
-            ),
-        )
-        rows = await cur.fetchall()
-        vers_list = []
-        for r in rows:
-            vers = str(r[0]) + "." + str(r[1]) + "." + str(r[2])
-            vers_list.append(vers)
-        return vers_list
+    return []
 
 
 async def get_vers_greater_than_eql(
@@ -148,38 +135,7 @@ async def get_vers_greater_than_eql(
     minor_vers: str,
     patch_vers: str,
 ) -> list[str]:
-    async with tx.cursor() as cur:
-        await cur.execute(
-            """
-                SELECT major_vers,
-                       minor_vers,
-                       patch_vers
-                FROM PACKAGE
-                WHERE repo_name=%s AND pkg_name=%s
-                  AND (major_vers>%s
-                       OR (major_vers=%s
-                           AND minor_vers>%s)
-                       OR (major_vers=%s
-                           AND minor_vers=%s
-                           AND patch_vers >=%s))
-            """,
-            (
-                repo_name,
-                pkg_name,
-                major_vers,
-                major_vers,
-                minor_vers,
-                major_vers,
-                minor_vers,
-                patch_vers,
-            ),
-        )
-        rows = await cur.fetchall()
-        vers_list = []
-        for r in rows:
-            vers = str(r[0]) + "." + str(r[1]) + "." + str(r[2])
-            vers_list.append(vers)
-        return vers_list
+    return []
 
 
 async def get_vers_greater_than(
@@ -190,38 +146,7 @@ async def get_vers_greater_than(
     minor_vers: str,
     patch_vers: str,
 ) -> list[str]:
-    async with tx.cursor() as cur:
-        await cur.execute(
-            """
-                SELECT major_vers,
-                       minor_vers,
-                       patch_vers
-                FROM PACKAGE
-                WHERE repo_name=%s AND pkg_name=%s
-                  AND (major_vers>%s
-                       OR (major_vers=%s
-                           AND minor_vers>%s)
-                       OR (major_vers=%s
-                           AND minor_vers=%s
-                           AND patch_vers >%s))
-            """,
-            (
-                repo_name,
-                pkg_name,
-                major_vers,
-                major_vers,
-                minor_vers,
-                major_vers,
-                minor_vers,
-                patch_vers,
-            ),
-        )
-        rows = await cur.fetchall()
-        vers_list = []
-        for r in rows:
-            vers = str(r[0]) + "." + str(r[1]) + "." + str(r[2])
-            vers_list.append(vers)
-        return vers_list
+    return []
 
 
 async def vers_exists(
@@ -232,31 +157,7 @@ async def vers_exists(
     minor_vers: str | int,
     patch_vers: str | int,
 ) -> bool:
-    async with tx.cursor() as cur:
-        await cur.execute(
-            """
-                SELECT COUNT(*)
-                FROM PACKAGE
-                WHERE repo_name=%s
-                AND pkg_name=%s
-                AND major_vers=%s
-                AND minor_vers=%s
-                AND patch_vers=%s
-            """,
-            (
-                repo_name,
-                pkg_name,
-                major_vers,
-                minor_vers,
-                patch_vers,
-            ),
-        )
-        row = await cur.fetchone()
-
-        if isinstance(row, tuple):
-            return int(row[0]) > 0
-        else:
-            return False
+    return []
 
 
 async def get_row_count(tx: Transaction) -> int:
