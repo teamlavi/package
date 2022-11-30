c5817e483350dc1a8e5c9374a5990215a8ddb23b
Tucker Siegel <tgsiegel@terpmail.umd.edu>
2022-11-29 23:49:36 -0500

    add sudos

--- scripts/vm/sync.sh
+++ scripts/vm/sync.sh
@@ -22,43 +22,42 @@ then
 fi
 
 build_lavi () {
-    mkdir -p "$PACKAGE_DIR/lavi-cli/ui/build" 
-    echo "<p>empty</p>" >> "$PACKAGE_DIR/lavi-cli/ui/build/index.html" 
+    sudo mkdir -p "$PACKAGE_DIR/lavi-cli/ui/build" 
     GOOS=$1 GOARCH=$2 go build -o lavi-cli-$1-$2
     if [[ $1 = "darwin" || $1 = "linux" ]]
     then
-        chmod +x lavi-cli-$1-$2
+        sudo chmod +x lavi-cli-$1-$2
     fi
 
-    mkdir "$PACKAGE_DIR/lavi-cli-$1-$2"
-    mv lavi-cli-$1-$2 "$PACKAGE_DIR/lavi-cli-$1-$2"
-    cp INSTALL.txt "$PACKAGE_DIR/lavi-cli-$1-$2"
+    sudo mkdir "$PACKAGE_DIR/lavi-cli-$1-$2"
+    sudo mv lavi-cli-$1-$2 "$PACKAGE_DIR/lavi-cli-$1-$2"
+    sudo cp INSTALL.txt "$PACKAGE_DIR/lavi-cli-$1-$2"
 
     if [[ $1 = "windows" ]]
     then
-        mv lavi-cli-$1-$2 lavi-cli-$1-$2.exe
+        sudo mv lavi-cli-$1-$2 lavi-cli-$1-$2.exe
     fi
-    zip -r "$PACKAGE_DIR/downloads/lavi/lavi-cli-$1-$2.zip" "$PACKAGE_DIR/lavi-cli-$1-$2"
-    rm -rf "$PACKAGE_DIR/lavi-cli-$1-$2"
+    sudo zip -r "$PACKAGE_DIR/downloads/lavi/lavi-cli-$1-$2.zip" "$PACKAGE_DIR/lavi-cli-$1-$2"
+    sudo rm -rf "$PACKAGE_DIR/lavi-cli-$1-$2"
 }
 
 build_lava () {
     GOOS=$1 GOARCH=$2 go build -o lava-cli-$1-$2
     if [[ $1 = "darwin" || $1 = "linux" ]]
     then
-        chmod +x lava-cli-$1-$2
+        sudo chmod +x lava-cli-$1-$2
     fi
 
-    mkdir "$PACKAGE_DIR/lava-cli-$1-$2"
-    mv lava-cli-$1-$2 "$PACKAGE_DIR/lava-cli-$1-$2"
-    cp INSTALL.txt "$PACKAGE_DIR/lava-cli-$1-$2"
+    sudo mkdir "$PACKAGE_DIR/lava-cli-$1-$2"
+    sudo mv lava-cli-$1-$2 "$PACKAGE_DIR/lava-cli-$1-$2"
+    sudo cp INSTALL.txt "$PACKAGE_DIR/lava-cli-$1-$2"
 
     if [[ $1 = "windows" ]]
     then
-        mv lava-cli-$1-$2 lava-cli-$1-$2.exe
+        sudo mv lava-cli-$1-$2 lava-cli-$1-$2.exe
     fi
-    zip -r "$PACKAGE_DIR/downloads/lava/lava-cli-$1-$2.zip" "$PACKAGE_DIR/lava-cli-$1-$2"
-    rm -rf "$PACKAGE_DIR/lava-cli-$1-$2"
+    sudo zip -r "$PACKAGE_DIR/downloads/lava/lava-cli-$1-$2.zip" "$PACKAGE_DIR/lava-cli-$1-$2"
+    sudo rm -rf "$PACKAGE_DIR/lava-cli-$1-$2"
 }
 
 cd "$PACKAGE_DIR" || exit 1
