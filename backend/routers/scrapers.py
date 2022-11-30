ed944cb8117864fb27c4957353b3f46081f03546
Levi Lutz <levi6900@icloud.com>
2022-11-29 21:57:02 -0500

    Add endpoints for other two scrapers

--- backend/routers/scrapers.py
+++ backend/routers/scrapers.py
@@ -20,10 +20,16 @@ def trigger_list_packages(repo_name: str, partial: bool) -> str:
 @router.post("/trigger/list_versions", response_class=PlainTextResponse)
 def trigger_list_versions(repo_name: str, package_name: str) -> str:
     """Trigger listing versions given a repo name and package name."""
-    ...
+    job = get_queue(QueueName.to_list_versions).enqueue(
+        scraping.list_package_versions, repo_name, package_name
+    )
+    return job.get_id()  # type: ignore
 
 
 @router.post("/trigger/generate_tree", response_class=PlainTextResponse)
 def trigger_generate_tree(repo_name: str, package_name: str, version: str) -> str:
     """Trigger generating tree, given repo, package, version."""
-    ...
+    job = get_queue(QueueName.to_generate_tree).enqueue(
+        scraping.generate_tree, repo_name, package_name, version
+    )
+    return job.get_id()  # type: ignore
