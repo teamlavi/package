93cd52341673ce7f46afc5da1f672ab701566928
Levi Lutz <levi6900@icloud.com>
2022-11-29 19:43:51 -0500

    Stub endpoints for scrapers

---
+++ backend/routers/scrapers.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+
+
+router = APIRouter(tags=["scrapers"])
+
+
+@router.post("/trigger/list_packages", response_class=PlainTextResponse)
+def trigger_list_packages(repo_name: str, limit: int | None) -> str:
+    """Trigger listing package names given a repo name."""
+    ...
+
+
+@router.post("/trigger/list_versions", response_class=PlainTextResponse)
+def trigger_list_versions(repo_name: str, package_name: str) -> str:
+    """Trigger listing versions given a repo name and package name."""
+    ...
+
+
+@router.post("/trigger/generate_tree", response_class=PlainTextResponse)
+def trigger_generate_tree(repo_name: str, package_name: str, version: str) -> str:
+    """Trigger generating tree, given repo, package, version."""
+    ...
